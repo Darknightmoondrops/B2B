7f4ed256866df3f8be843f6a6f7386745ac8d997
MM-Sharghi <mehrabani7798@gmail.com>
2022-03-04 22:36:03 +0330

    menu and fonts edited

--- js/index.js
+++ js/index.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-=======
-//===========================slider=========================
-var slideIndex = 1;
-showSliders(slideIndex);
-
-//dot-btn => image controls
-function currentSlide(n) {
-    showSliders(slideIndex = n);
-}
-
-function showSliders(n) {
-    var i;
-    var slides = document.getElementsByClassName("carousel-item");
-    var dotsBtn = document.getElementsByClassName("dot-btn");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dotsBtn.length; i++) {
-        dotsBtn[i].classList.remove("slide-active");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dotsBtn[slideIndex - 1].classList.add("slide-active");
-}
-
-
-
->>>>>>> fcfc28be15168e170f675f4a5b61bbe58bd813a7
 
 
 // ======================MENU=====================
@@ -57,8 +27,14 @@ function openTabs(el) {
 
     btnTarget.classList.add("active");
 }
+
+function menufunc() {
+    var toggeler =  document.getElementById("menu-list").style.display
+    if(toggeler== "none") document.getElementById("menu-list").style.display = "flex";
+    if(toggeler== "flex") document.getElementById("menu-list").style.display = "none";
+
+}
 // ======================MENU=====================
-<<<<<<< HEAD
 // ======================SEARCH_MENU==============
 function myFunction() {
     document.getElementById("search-box").style.display = "block";
@@ -68,8 +44,6 @@ function myFunction() {
   }   
 // ======================SEARCH_MENU==============
 
-=======
->>>>>>> fcfc28be15168e170f675f4a5b61bbe58bd813a7
 
 
 
