787c8ac7024b268f9ff4d45d2a53ed5ccce84870
MM-Sharghi <mehrabani7798@gmail.com>
2022-03-06 12:00:40 +0330

    about and contct page added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+// ==========================MAP===========================
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
 //===========================slider=========================
 var slideIndex = 1;
 showSliders(slideIndex);
