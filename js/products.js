93423274c39634325cd621a8fe9c14595468c971
arian soleimanzadeh <Arian@DESKTOP-GOKUOOQ.Zone_Arian.com>
2022-03-07 22:17:27 +0330

    edit+allsliders

--- js/products.js
+++ js/products.js
@@ -25,6 +25,9 @@ function slidechenger5(n) {
     }
     page[pagenumber5 - 1].style.display = "block";
     number[pagenumber5 - 1].classList.add("dot-active");
+    setInterval(function() {
+        showSliders(slideIndex = slideIndex + 1)
+    }, 5000);
 }
 //=========================================== slider product 6 ===========================
 var pagenumber6 = 1;
@@ -81,6 +84,67 @@ function slidechenger7(n) {
     }
     page[pagenumber7 - 1].style.display = "block";
     number[pagenumber7 - 1].classList.add("dot-active");
+}
+//======================================== change page horiz ==============================================
+
+var pagenumber = 1;
+pagechenger(pagenumber);
+
+
+function pluspage(n) {
+    pagechenger(pagenumber += n)
+}
+
+function pageslide(n) {
+    pagechenger(pagenumber = n);
+}
+
+function pagechenger(n) {
+    var i;
+    var page = document.getElementsByClassName("products-page");
+
+    var number = document.getElementsByClassName("page");
+    if (n > page.length) { pagenumber = 1 }
+    if (n < 1) { pagenumber = page.length }
+    for (i = 0; i < page.length; i++) {
+        page[i].style.display = "none";
+    }
+    for (i = 0; i < number.length; i++) {
+        number[i].classList.remove("pageActive");
+    }
+    page[pagenumber - 1].style.display = "block";
+    number[pagenumber - 1].classList.add("pageActive");
+}
+
+
+//============================== change page vert ================
+var pagenumber8 = 1;
+pagechengers(pagenumber8);
+
+
+function pluspages(n) {
+    pagechengers(pagenumber8 += n)
+}
+
+function pageslides(n) {
+    pagechengers(pagenumber8 = n);
+}
+
+function pagechengers(n) {
+    var i;
+    var pageVert = document.getElementsByClassName("products-page1");
+
+    var number1 = document.getElementsByClassName("pages");
+    if (n > pageVert.length) { pagenumber8 = 1 }
+    if (n < 1) { pagenumber8 = pageVert.length }
+    for (i = 0; i < pageVert.length; i++) {
+        pageVert[i].style.display = "none";
+    }
+    for (i = 0; i < number1.length; i++) {
+        number1[i].classList.remove("pageActive");
+    }
+    pageVert[pagenumber8 - 1].style.display = "block";
+    number1[pagenumber8 - 1].classList.add("pageActive");
 }
 //=============================price range=======================
 const rangeInput = document.querySelectorAll(".multi-range input"),
@@ -144,38 +208,4 @@ function changepage(n) {
         bordshow1[0].classList.remove("show-mode-border");
         bordshow2[0].classList.add("show-mode-border");
     }
-}
-
-
-
-
-
-
-//======================================== change page ==============================================
-var pagenumber = 1;
-pagechenger(pagenumber);
-
-
-function pluspage(n) {
-    pagechenger(pagenumber += n)
-}
-
-function pageslide(n) {
-    pagechenger(pagenumber = n);
-}
-
-function pagechenger(n) {
-    var i;
-    var page = document.getElementsByClassName("products-page");
-    var number = document.getElementsByClassName("page");
-    if (n > page.length) { pagenumber = 1 }
-    if (n < 1) { pagenumber = page.length }
-    for (i = 0; i < page.length; i++) {
-        page[i].style.display = "none";
-    }
-    for (i = 0; i < number.length; i++) {
-        number[i].classList.remove("pageActive");
-    }
-    page[pagenumber - 1].style.display = "block";
-    number[pagenumber - 1].classList.add("pageActive");
 }
