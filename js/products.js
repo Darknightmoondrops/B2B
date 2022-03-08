75164c63c5796fd6a174ed02b09f70fd4d4aa934
arian soleimanzadeh <Arian@DESKTOP-GOKUOOQ.Zone_Arian.com>
2022-03-08 11:08:17 +0330

    edit filter+

--- js/products.js
+++ js/products.js
@@ -85,6 +85,63 @@ function slidechenger7(n) {
     page[pagenumber7 - 1].style.display = "block";
     number[pagenumber7 - 1].classList.add("dot-active");
 }
+//================================================  colors ==============================================
+// ============ 1 =============
+function pink() {
+    var i;
+    var color1 = document.getElementsByClassName("colors");
+    for (i = 0; i < color1.length; i++) {
+        color1[i].style.display = "none";
+    }
+    color1[0].style.display = "block";
+}
+// ============ 2 =============
+function blue() {
+    var i;
+    var color2 = document.getElementsByClassName("colors");
+    for (i = 0; i < color2.length; i++) {
+        color2[i].style.display = "none";
+    }
+    color2[1].style.display = "block";
+}
+// ============ 3 =============
+function yellow() {
+    var i;
+    var color3 = document.getElementsByClassName("colors");
+    for (i = 0; i < color3.length; i++) {
+        color3[i].style.display = "none";
+    }
+    color3[2].style.display = "block";
+}
+// ============ 4 =============
+function green() {
+    var i;
+    var color4 = document.getElementsByClassName("colors");
+    for (i = 0; i < color4.length; i++) {
+        color4[i].style.display = "none";
+    }
+    color4[3].style.display = "block";
+}
+// ============ 5 =============
+function purple() {
+    var i;
+    var color5 = document.getElementsByClassName("colors");
+    for (i = 0; i < color5.length; i++) {
+        color5[i].style.display = "none";
+    }
+    color5[4].style.display = "block";
+}
+// ============ 6 =============
+function red() {
+    var i;
+    var color6 = document.getElementsByClassName("colors");
+    for (i = 0; i < color6.length; i++) {
+        color6[i].style.display = "none";
+    }
+    color6[5].style.display = "block";
+}
+
+
 //======================================== change page horiz ==============================================
 
 var pagenumber = 1;
