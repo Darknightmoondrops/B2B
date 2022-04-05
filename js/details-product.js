75ec9a4989425f88ad932a3bfd9bc6e65db87fc7
arian soleimanzadeh <Arian@DESKTOP-GOKUOOQ.Zone_Arian.com>
2022-04-05 14:46:57 +0430

    slider in 2 details page

--- js/details-product.js
+++ js/details-product.js
@@ -60,37 +60,38 @@ var count = 1
 function plus() {
     document.getElementById('counter').innerHTML = ++count
 }
+
 function minus() {
     document.getElementById('counter').innerHTML = --count
 }
 
 
 // =================================== send ======================
-function message(type,msg) {
+function message(type, msg) {
     var succ = "error"
     var messageText = " ارسال شد"
     var snackbar = document.getElementById('snackbar')
-    
-    if(succ == "sucsses"){
+
+    if (succ == "sucsses") {
         snackbar.innerText = messageText
         snackbar.className += " sucsses"
-    setTimeout(function(){
-        snackbar.innerText = " "
-         snackbar.classList.remove("sucsses")
+        setTimeout(function() {
+            snackbar.innerText = " "
+            snackbar.classList.remove("sucsses")
         }, 3000);
-}
-   
-if(succ == "error"){
-    snackbar.innerText = "ارسال نشد"
-    snackbar.className += " error"
-setTimeout(function(){
-    snackbar.innerText = " "
-     snackbar.classList.remove("error")
-    }, 3000);
-}
+    }
+
+    if (succ == "error") {
+        snackbar.innerText = "ارسال نشد"
+        snackbar.className += " error"
+        setTimeout(function() {
+            snackbar.innerText = " "
+            snackbar.classList.remove("error")
+        }, 3000);
+    }
 
     // setTimeout(function(){ snackbar.classList.remove("error")}, 3000);
-  }
+}
 
 
 var pagenumber1 = 1;
--- js/products.js
+++ js/products.js
@@ -155,7 +155,7 @@ function searchBrand() {
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("p")[0];
+        a = li[i].getElementsByTagName("p")[0, 1];
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
